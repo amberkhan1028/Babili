94cbc64bb741293014e557c752483b92e65dc6da
Amber Khan <4access2amber@gmail.com>
2020-12-21 09:18:40 -0600

    (create) navbar and profile page front end

---
+++ client/src/screens/CameraScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const CameraScreen = () => (
+  <View>
+    <Text>Camera</Text>
+  </View>
+);
+
+// const styles = StyleSheet.create({});
+
+export default CameraScreen;
---
+++ client/src/screens/MessagesScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const MessagesScreen = () => (
+  <View>
+    <Text>Messages</Text>
+  </View>
+);
+
+// const styles = StyleSheet.create({});
+
+export default MessagesScreen;
