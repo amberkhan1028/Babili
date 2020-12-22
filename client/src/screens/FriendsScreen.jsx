4b5df12355e814fe46ef6b02438e7066e57e071f
Amber Khan <4access2amber@gmail.com>
2020-12-21 19:21:19 -0600

    (updated) friend basic front end

--- client/src/screens/FriendsScreen.jsx
+++ client/src/screens/FriendsScreen.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import { Searchbar } from 'react-native-paper';
 
 const FriendsScreen = () => (
   <View>
     <Text>Friends</Text>
-    <Searchbar
-      placeholder="search for friend"
-    />
   </View>
 );
 
--- client/src/screens/LoginScreen.jsx
+++ client/src/screens/LoginScreen.jsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import {
   View, Text, StyleSheet, Image, StatusBar,
@@ -23,24 +22,6 @@ const styles = StyleSheet.create({
   },
 });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fec857',
-  },
-  logo: {
-    width: 200,
-    height: 200,
-  },
-  logoContainer: {
-    alignItems: 'center',
-    flexGrow: 1,
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 50,
-  },
-});
 const LoginScreen = () => (
   <View style={styles.container}>
     <StatusBar
