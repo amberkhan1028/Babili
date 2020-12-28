24765ea62f75247e15de0e9e91f869ad40ad05eb
Jazmine <jazminejfox@gmail.com>
2020-12-28 08:57:57 -0600

    (create) messages routes and pusher config

--- server/index.js
+++ server/index.js
@@ -2,17 +2,21 @@
 /* eslint-disable no-console */
 const express = require('express');
 const { Client } = require('pg');
+
 const { sequelize } = require('./db/index');
 const wordbank = require('./db/Routes/wordBank');
+const messages = require('./db/Routes/messages');
 
 const app = express();
 app.use(express.json());
 app.use('/', wordbank);
+app.use('/', messages);
 const PORT = process.env.PORT || 3000;
 const connectionString = process.env.CONNECTION_STRING;
 const db = new Client({
   connectionString,
 });
+
 async function connect() {
   try {
     await db.connect();
