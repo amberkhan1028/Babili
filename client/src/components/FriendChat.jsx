f9b098ca062e92954170b4f0ff3ea9036b49b12e
ZiaTahir504 <mariatahir504@gmail.com>
2021-01-07 19:30:33 -0600

    (feat) big text for friend chat

--- client/src/components/FriendChat.jsx
+++ client/src/components/FriendChat.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-shadow */
 /* eslint-disable no-underscore-dangle */
@@ -10,9 +11,10 @@ import {
   Keyboard,
   View,
   KeyboardAvoidingView,
+  Image,
 } from 'react-native';
 import Pusher from 'pusher-js/react-native';
-import { GiftedChat } from 'react-native-gifted-chat';
+import { GiftedChat, Actions, Bubble } from 'react-native-gifted-chat';
 import axios from 'axios';
 import pusherConfig from '../../../pusher.json';
 
@@ -24,6 +26,8 @@ const styles = StyleSheet.create({
 });
 const FriendChat = ({ currentFriend, sender }) => {
   const [messages, setMessages] = React.useState([]);
+  const [fonts, setFonts] = React.useState(15);
+
   React.useEffect(() => {
     const pusher = new Pusher(pusherConfig.key, pusherConfig);
     const chatChannel = pusher.subscribe('chat_channel');
@@ -46,13 +50,61 @@ const FriendChat = ({ currentFriend, sender }) => {
       const { data } = await axios.get(`${pusherConfig.serverEndpoint}/users/messages/?sender=${sender.email}&receiver=${currentFriend.email}`);
       setMessages(data);
     })();
-  }, [currentFriend, messages]);
+  }, [currentFriend, messages, fonts]);
+
+  const changeFontSize = () => {
+    (fonts === 15 ? setFonts(28) : setFonts(15));
+  };
+
+  const renderActions = (props) => (
+    <Actions
+      {...props}
+      containerStyle={{
+        width: 44,
+        height: 44,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginLeft: 4,
+        marginRight: 4,
+        marginBottom: 0,
+      }}
+      icon={() => (
+        <Image
+          style={{ width: 32, height: 32 }}
+          source={{
+            uri: 'https://icon-library.com/images/font-size-icon/font-size-icon-18.jpg',
+          }}
+        />
+      )}
+      onPressActionButton={() => { changeFontSize(); }}
+    />
+  );
+
+  const renderBubble = (props) => (
+    <Bubble
+      {...props}
+      wrapperStyle={{
+        right: {
+          backgroundColor: '#0f9535',
+        },
+      }}
+      textStyle={{
+        right: {
+          fontSize: fonts,
+        },
+        left: {
+          fontSize: fonts,
+        },
+      }}
+    />
+  );
 
   const onSend = React.useCallback((messages = []) => {
     const receiver = { _id: currentFriend.email, name: currentFriend.username || '', avatar: currentFriend.image };
     const message = messages[0];
     axios.post(`${pusherConfig.serverEndpoint}/users/messages`, { message, receiver });
   }, []);
+
   return (
     <TouchableWithoutFeedback onPress={() => { Keyboard.dismiss(); }}>
       <View style={{ flex: 2 }}>
@@ -65,6 +117,8 @@ const FriendChat = ({ currentFriend, sender }) => {
             onSend={(messages) => onSend(messages)}
             user={{ _id: sender.email, name: sender.username, avatar: sender.image }}
             showUserAvatar
+            renderActions={renderActions}
+            renderBubble={renderBubble}
           />
         </KeyboardAvoidingView>
 
